ba9f489b3bd67dcd5d308c230a23accecb63509c
tswaincae1221 <125733195+tswaincae1221@users.noreply.github.com>
2026-08-19 01:44:50 +0900

    export season trend features

--- src/hgb_feature_selection_v2.py
+++ src/hgb_feature_selection_v2.py
@@ -1,4 +1,5 @@
 from .hgb_feature_selection_v2_pkg.common import *  # noqa: F401,F403
+from .hgb_feature_selection_v2_pkg.season_trend import *  # noqa: F401,F403
 from .hgb_feature_selection_v2_pkg.trackman import *  # noqa: F401,F403
 from .hgb_feature_selection_v2_pkg.selection import *  # noqa: F401,F403
 from .hgb_feature_selection_v2_pkg.runner import main, parse_args, run_pipeline
